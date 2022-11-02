210945625ac4446448dacea6b5dc50933b30290d
mdesabd <esa.ce2020@gmail.com>
2022-11-02 20:26:25 +0600

    aos add

---
+++ aos.js
@@ -0,0 +1,7 @@
+AOS.init({
+    offset: 300, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 1500, // values from 0 to 3000, with step 50ms
+    mirror: false,
+    anchorPlacement: 'top-bottom',
+});
